b15bffde9b3cd8cb9f9af07db987aa3537e5365f
Travis <me@traviswebb.com>
2016-09-01 14:42:10 -0400

    [test] remove pool config test

--- test/trailpack.test.js
+++ test/trailpack.test.js
@@ -3,14 +3,6 @@
 const assert = require('assert')
 
 describe('Knex Trailpack', () => {
-  describe('#configure', () => {
-    it('should set default pool config if not set', () => {
-      const stores = global.app.config.database.stores
-      assert(stores.teststore.pool)
-      assert(stores.teststore.pool.min > 0)
-      assert(stores.teststore.pool.max > 0)
-    })
-  })
   describe('#initialize', () => {
     it('should group stores and models', () => {
       const stores = global.app.packs.knex.stores
